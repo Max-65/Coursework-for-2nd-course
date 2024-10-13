0db146fb9cb0377b4aa3438ab289e1f2e87f497c
Max-65 <macsim.bazdyrev@gmail.com>
2024-10-13 22:10:56 +0300

    Update matlib.cpp

--- matlib.cpp
+++ matlib.cpp
@@ -1,65 +1,63 @@
-#pragma once
+#include <iostream>
+#include "matlib.h"
 
-namespace matlib {
-	class Array {
-	private:
-		int size_;
-		double* arr;
-	public:
-		Array(int size = 0, double allocator = 0.0) : size_(size) {
-			arr = new double[size_];
-			for (int i = 0; i < size_; ++i) {
-				arr[i] = allocator;
-			}
-		}
-		~Array() {
-			delete[] arr;
-			arr = nullptr;
-		}
-		double& operator[](int i) const;
-		void input();
-		int size() const;
-	};
-/*
-	vector x3:
-	v.x
-	v.y
-	v.z
-*/
-	class vec3 {
-	public:
-		double x, y, z;
-		vec3(double value = 0.0) : x(value), y(value), z(value) {}
-		vec3(double x, double y, double z) : x(x), y(y), z(z) {}
-		vec3(vec3& v) : x(v.x), y(v.y), z(v.z) {}
-		vec3& operator+=(vec3& v);
-	};
+// Array class ----------------------------------------------------------------
+double& matlib::Array::operator[](int i) const {
+	return arr[i];
+}
+void matlib::Array::input() {
+	for (int i = 0; i < size_; ++i) {
+		std::cin >> arr[i];
+	}
+}
+int matlib::Array::size() const {
+	return size_;
+}
 
-/*
-	matrix 3x3:
-	a.x b.x c.x
-	a.y b.y c.y
-	a.z b.z c.z
+// Vector x3 class ------------------------------------------------------------
+matlib::vec3& matlib::vec3::operator+=(vec3& v) {
+	x += v.x;
+	y += v.y;
+	z += v.z;
+	return *this;
+}
 
-	transposed matrix 3x3:
-	a.x a.y a.z
-	b.x b.y b.z
-	c.x c.y c.z
-	
-	Note: transposed matrix is simpler to be sorted by column values - just by swapping vectors
-*/
-	class mat3 {
-	private:
-		vec3 a, b, c;
-		void sort(int rang);
-	public:
-		mat3(double value = 0.0) : a(value, 0.0, 0.0), b(0.0, value, 0.0), c(0.0, 0.0, value) {}
-		mat3(vec3& a, vec3& b, vec3& c) : a(a), b(b), c(c) {}
-		mat3(mat3& m) : a(m.a), b(m.b), c(m.c) {}
-		mat3& operator+=(mat3& m);
-		void transpose();
-	};
+// Matrix 3x3 class -----------------------------------------------------------
+void matlib::mat3::transpose() {
+	std::swap(a.y, b.x);
+	std::swap(a.z, c.x);
+	std::swap(b.z, c.y);
+}
+void matlib::mat3::sort(int rang) {
+	if (rang == 3) {
 
-	void EvalCoef(Array& x, Array& y, matlib::mat3& A, matlib::vec3& b);
-	void Solve_SLAE(mat3& A, vec3& b, vec3& c);
+	}
+}
+matlib::mat3& matlib::mat3::operator+=(mat3& m) {
+	a += m.a;
+	b += m.b;
+	c += m.c;
+	return *this;
+}
+
+// Math library functions -----------------------------------------------------
+void matlib::EvalCoef(Array& x, Array& y, mat3& A, vec3& b) {
+	vec3 v1, v2, v3;
+	for (int i = 0; i < x.size(); ++i) {
+		v1.x += 1;
+		v2.x += x[i];
+		v2.y += x[i] * x[i];
+		v3.x += std::exp(-x[i]);
+		v3.y += std::exp(-x[i]) * x[i];
+		v3.z += std::exp(-x[i] * 2);
+
+		b.x += y[i];
+		b.y += y[i] * x[i];
+		b.z += y[i] * std::exp(-x[i]);
+	}
+	mat3 tmp(v1, v2, v3);
+	A += tmp;
+}
+void matlib::Solve_SLAE(mat3& A, vec3& b, vec3& c) {
+
 }
