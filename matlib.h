52539be8d6c151f3b9c79d35aa832e82f2f7b4c8
Max-65 <macsim.bazdyrev@gmail.com>
2024-10-14 15:13:06 +0300

    Update matlib.h

--- matlib.h
+++ matlib.h
@@ -51,15 +51,37 @@ namespace matlib {
 	class mat3 {
 	private:
 		vec3 a, b, c;
-		void sort(int rang);
+		
 	public:
-		mat3(double value = 0.0) : a(value, 0.0, 0.0), b(0.0, value, 0.0), c(0.0, 0.0, value) {}
-		mat3(vec3& a, vec3& b, vec3& c) : a(a), b(b), c(c) {}
-		mat3(mat3& m) : a(m.a), b(m.b), c(m.c) {}
+		double&
+			a11, a12, a13,
+			a21, a22, a23,
+			a31, a32, a33;
+		mat3(double value = 0.0) :
+			a(value, 0.0, 0.0), b(0.0, value, 0.0), c(0.0, 0.0, value),
+			a11(a.x), a12(b.x), a13(c.x),
+			a21(a.y), a22(b.y), a23(c.y),
+			a31(a.z), a32(b.z), a33(c.z)
+		{}
+		mat3(vec3& a, vec3& b, vec3& c) :
+			a(a), b(b), c(c),
+			a11(a.x), a12(b.x), a13(c.x),
+			a21(a.y), a22(b.y), a23(c.y),
+			a31(a.z), a32(b.z), a33(c.z)
+		{}
+		mat3(mat3& m) :
+			a(m.a), b(m.b), c(m.c),
+			a11(a.x), a12(b.x), a13(c.x),
+			a21(a.y), a22(b.y), a23(c.y),
+			a31(a.z), a32(b.z), a33(c.z)
+		{}
 		mat3& operator+=(mat3& m);
 		void transpose();
+		void sort(int rang);
 	};
 
+	vec3 operator*(vec3& vector, double value);
+	vec3 operator*(double value, vec3& vector);
 	void EvalCoef(Array& x, Array& y, matlib::mat3& A, matlib::vec3& b);
 	void Solve_SLAE(mat3& A, vec3& b, vec3& c);
 }
