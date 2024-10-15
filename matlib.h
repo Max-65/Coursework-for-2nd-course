2e926100e5aa3b0a646a0f9060f02de708bef998
Max-65 <macsim.bazdyrev@gmail.com>
2024-10-15 23:48:32 +0300

    Update matlib.h

--- matlib.h
+++ matlib.h
@@ -31,8 +31,13 @@ namespace matlib {
 		double x, y, z;
 		vec3(double value = 0.0) : x(value), y(value), z(value) {}
 		vec3(double x, double y, double z) : x(x), y(y), z(z) {}
-		vec3(vec3& v) : x(v.x), y(v.y), z(v.z) {}
-		vec3& operator+=(vec3& v);
+		vec3(vec3 const& v) : x(v.x), y(v.y), z(v.z) {}
+		vec3& operator=(vec3 const& v);
+		vec3 operator+() const;
+		vec3 operator-() const;
+		vec3& operator+=(vec3 const& v);
+		vec3& operator-=(vec3 const& v);
+		void reverse();
 	};
 
 /*
@@ -51,37 +56,42 @@ namespace matlib {
 	class mat3 {
 	private:
 		vec3 a, b, c;
-		
 	public:
-		double&
-			a11, a12, a13,
-			a21, a22, a23,
-			a31, a32, a33;
+		double
+			&a11, &a12, &a13,
+			&a21, &a22, &a23,
+			&a31, &a32, &a33;
 		mat3(double value = 0.0) :
 			a(value, 0.0, 0.0), b(0.0, value, 0.0), c(0.0, 0.0, value),
 			a11(a.x), a12(b.x), a13(c.x),
 			a21(a.y), a22(b.y), a23(c.y),
 			a31(a.z), a32(b.z), a33(c.z)
 		{}
-		mat3(vec3& a, vec3& b, vec3& c) :
-			a(a), b(b), c(c),
+		mat3(vec3 const& v1, vec3 const& v2, vec3 const& v3) :
+			a(v1), b(v2), c(v3),
 			a11(a.x), a12(b.x), a13(c.x),
 			a21(a.y), a22(b.y), a23(c.y),
 			a31(a.z), a32(b.z), a33(c.z)
 		{}
-		mat3(mat3& m) :
+		mat3(mat3 const& m) :
 			a(m.a), b(m.b), c(m.c),
 			a11(a.x), a12(b.x), a13(c.x),
 			a21(a.y), a22(b.y), a23(c.y),
 			a31(a.z), a32(b.z), a33(c.z)
 		{}
-		mat3& operator+=(mat3& m);
+
+		mat3& operator=(mat3 const& m);
+		mat3 operator+() const;
+		mat3 operator-() const;
+		mat3& operator+=(mat3 const& m);
+		mat3& operator-=(mat3 const& m);
 		void transpose();
-		void sort(int rang);
+		void reverse();
+		void sort(vec3& v, int rang);
+		void print() const;
 	};
 
-	vec3 operator*(vec3& vector, double value);
-	vec3 operator*(double value, vec3& vector);
-	void EvalCoef(Array& x, Array& y, matlib::mat3& A, matlib::vec3& b);
+	vec3 operator*(vec3 const& vector, double value);
+	void EvalCoef(Array& x, Array& y, mat3& A, vec3& b);
 	void Solve_SLAE(mat3& A, vec3& b, vec3& c);
 }
