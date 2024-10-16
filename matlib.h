5cc76732ce8f193bc788d2a670a8673c43411412
Max-65 <macsim.bazdyrev@gmail.com>
2024-10-16 23:57:40 +0300

    Update matlib.h

--- matlib.h
+++ matlib.h
@@ -16,7 +16,7 @@ namespace matlib {
 			delete[] arr;
 			arr = nullptr;
 		}
-		double& operator[](int i) const;
+		double& operator[](int const& i) const;
 		int size() const;
 		friend std::istream& operator>>(std::istream& is, Array& arr);
 		friend std::ostream& operator<<(std::ostream& os, Array const& arr);
@@ -28,16 +28,21 @@ namespace matlib {
 	v.z
 */
 	class vec3 {
+	private:
+		int size_;
 	public:
 		double x, y, z;
-		vec3(double value = 0.0) : x(value), y(value), z(value) {}
-		vec3(double x, double y, double z) : x(x), y(y), z(z) {}
-		vec3(vec3 const& v) : x(v.x), y(v.y), z(v.z) {}
+		vec3(double value = 0.0) : x(value), y(value), z(value), size_(3) {}
+		vec3(double x, double y, double z) : x(x), y(y), z(z), size_(3) {}
+		vec3(vec3 const& v) : x(v.x), y(v.y), z(v.z), size_(3) {}
 		vec3& operator=(vec3 const& v);
+		double& operator[](int const& i);
+		double const& operator[](int const& i) const;
 		vec3 operator+() const;
 		vec3 operator-() const;
 		vec3& operator+=(vec3 const& v);
 		vec3& operator-=(vec3 const& v);
+		int size() const;
 		void reverse();
 	};
 
@@ -57,44 +62,27 @@ namespace matlib {
 	class mat3 {
 	private:
 		vec3 a, b, c;
+		int size_;
 	public:
-		double
-			&a11, &a12, &a13,
-			&a21, &a22, &a23,
-			&a31, &a32, &a33;
-		mat3(double value = 0.0) :
-			a(value, 0.0, 0.0), b(0.0, value, 0.0), c(0.0, 0.0, value),
-			a11(a.x), a12(b.x), a13(c.x),
-			a21(a.y), a22(b.y), a23(c.y),
-			a31(a.z), a32(b.z), a33(c.z)
-		{}
-		mat3(vec3 const& v1, vec3 const& v2, vec3 const& v3) :
-			a(v1), b(v2), c(v3),
-			a11(a.x), a12(b.x), a13(c.x),
-			a21(a.y), a22(b.y), a23(c.y),
-			a31(a.z), a32(b.z), a33(c.z)
-		{}
-		mat3(mat3 const& m) :
-			a(m.a), b(m.b), c(m.c),
-			a11(a.x), a12(b.x), a13(c.x),
-			a21(a.y), a22(b.y), a23(c.y),
-			a31(a.z), a32(b.z), a33(c.z)
-		{}
+		mat3(double value = 0.0) : a(value, 0.0, 0.0), b(0.0, value, 0.0), c(0.0, 0.0, value), size_(3)	{}
+		mat3(vec3 const& v1, vec3 const& v2, vec3 const& v3) : a(v1), b(v2), c(v3), size_(3) {}
+		mat3(mat3 const& m) : a(m.a), b(m.b), c(m.c), size_(3) {}
 
 		mat3& operator=(mat3 const& m);
+		vec3& operator[](int const& i);
+		vec3 const& operator[](int const& i) const;
 		mat3 operator+() const;
 		mat3 operator-() const;
 		mat3& operator+=(mat3 const& m);
 		mat3& operator-=(mat3 const& m);
+		int size() const;
 		void transpose();
 		void reverse();
-		void sort(vec3& v, int rang);
+		void swapMaxEquation(vec3& v, int rang);
 		friend std::ostream& operator<<(std::ostream& os, mat3 const& m);
 	};
 
-	double f1(double const& xi);
-	double f2(double const& xi);
-	double f3(double const& xi);
+	double f(int const& i, double const& xi);
 	vec3 operator*(vec3 const& vector, double value);
 	void EvalCoef(Array const& x, Array const& y, mat3& A, vec3& b);
 	void SolveSLAE(mat3& A, vec3& b, vec3& c);
